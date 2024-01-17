cdd1d7d5e276de224b8634db558a728ef1e03b4c
Max Bondarenco <zeetreex@gmail.com>
2024-01-17 14:40:26 +0200

    config decomposition

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,59 +1,27 @@
 import path from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server"
+import type { BuildMode } from "./config/build/types/types";
+import { buildWebpack } from "./config/build/buildWebpack";
 
-type Mode = "production" | "development"
 
 interface EnvVariables {
-	mode: Mode;
+	mode: BuildMode;
 	port: number;
 }
 
 export default (env: EnvVariables) => {
 	const isDev = env.mode === "development";
-	const isProd = !isDev;
 
-	const config: webpack.Configuration = {
-		mode: env.mode ?? "development",
-		entry: path.resolve(__dirname, "src", "index.tsx"),
-		output: {
-			path: path.resolve(__dirname, "build"),
-			filename: "[name].[contenthash].js",
-			clean: true,
-		},
-		plugins: [
-			new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") }),
-			// slow plugin
-			isDev && new webpack.ProgressPlugin(),
-			isProd && new MiniCssExtractPlugin({
-				filename: "css/[name].[contenthash:8].css",
-				chunkFilename: "css/[name].[contenthash:8].css"
-			})
-		].filter(Boolean),
-		module: {
-			rules: [
-				{
-					test: /\.s([ac])ss$/i,
-					use: [isDev ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-				},
-				{
-					test: /\.tsx?$/,
-					use: "ts-loader",
-					exclude: /node_modules/,
-				},
-			],
-		},
-		resolve: {
-			extensions: [".tsx", ".ts", ".js"],
-		},
-		devServer: isDev ? {
-			port: env.port ?? 5555,
-			open: true
-		} : undefined,
-		devtool: isDev && "inline-source-map"
+	const paths = {
+		html: path.resolve(__dirname, "public", "index.html"),
+		output: path.resolve(__dirname, "build"),
+		entry: path.resolve(__dirname, "src", "index.tsx")
 	};
+
+	const config = buildWebpack({
+		mode: env.mode ?? "development",
+		port: 7777 ?? 5555,
+		paths
+	});
 
 	return config;
 };
